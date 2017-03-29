858f1d2a5aca4d435c65ff8a3825739d8f1c0bf1
Paraprax <parkerjohnston28@gmail.com>
2017-03-29 17:24:09 -0400

    finishing cat speeches program :)

--- Cat_Class_Project/cat.rb
+++ Cat_Class_Project/cat.rb
@@ -9,8 +9,24 @@ class Cat
     attr_accessor :preferred_food
     attr_accessor :meal_time
 
+
+  def eats_at
+    puts "#{meal_time} PM"
+  end
+
+  def meow
+    puts "My name is #{name}, my preferred food is #{preferred_food}
+    and my meal time is #{meal_time}!"
+  end
+
 end
 
-cat1 = Cat.new('Bottle','salmon','7pm')
+  cat1 = Cat.new('Bottle','salmon','19')
 
-cat2 = Cat.new('Knuffler','tuna','7:30pm')
+  cat2 = Cat.new('Knuffler','tuna', '19:30')
+
+
+  puts cat1.eats_at
+
+  puts  cat1.meow
+  puts  cat2.meow
