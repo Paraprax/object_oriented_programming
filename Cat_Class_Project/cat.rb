095c4e1bf74dd74f3ebeffffef461ee81755fbd4
Paraprax <parkerjohnston28@gmail.com>
2017-03-29 17:05:24 -0400

    added two cats

--- Cat_Class_Project/cat.rb
+++ Cat_Class_Project/cat.rb
@@ -1,6 +1,16 @@
 class Cat
-  def initialize(name, preferred_food, meal_time)
-    @name = name
-    @preferred_food = preferred_food
-    @meal_time = meal_time
+    def initialize(name, preferred_food, meal_time)
+      @name = name
+      @preferred_food = preferred_food
+      @meal_time = meal_time
+    end
+
+    attr_accessor :name
+    attr_accessor :preferred_food
+    attr_accessor :meal_time
+
 end
+
+cat1 = Cat.new('Bottle','salmon','7pm')
+
+cat2 = Cat.new('Knuffler','tuna','7:30pm')
