caa2a36d76a2be2c45941390a11aae54708fee44
Paraprax <parkerjohnston28@gmail.com>
2017-03-30 15:48:03 -0400

    Finished Nintendo's 'Paperboy 3: The Hard Way' for SuperNES.

--- paperboy.rb
+++ paperboy.rb
@@ -9,19 +9,30 @@ class Paperboy
   attr_reader :papers_delivered
   attr_reader :earnings
 
-  def deliver(start_address, end_address)
-     route = end_address - start_address + 1
-     @earnings += route*0.25
-     @papers_delivered = route
-  end
-
   def quota
     experience = @papers_delivered/2
     experience += 50
   end
 
+  def deliver(start_address, end_address)
+     route = end_address - start_address + 1
+     extra_deliveries = route - quota
+     base_deliveries = route - extra_deliveries
+     #should earn 50 cents, not 25, for each paper delivered after the quota is met
+     if route > quota
+       @earnings += extra_deliveries*0.50 + base_deliveries*0.25
+       puts "Extra deliveries: #{extra_deliveries} papers!"
+     else
+       @earnings -= 2
+       @earnings += route*0.25
+     end
+       @papers_delivered += route
+       puts "Base deliveries: #{route} papers!"
+       puts "#{name} made #{earnings} dollars today! Tomorrow's quota: #{quota}!"
+  end
+
   def report
-    puts "My name is #{name}, I've delivered #{papers_delivered} and made #{earnings} dollars from it!"
+    puts "My name is #{name}, I've delivered #{papers_delivered} papers and made #{earnings} dollars from it!"
   end
 
 end
